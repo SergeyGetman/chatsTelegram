611db433cf2c4789e51a8be63080989fe0239cdf
Sergey <biznesgroup2017@gmail.com>
2021-06-08 19:39:49 +0300

    add class

--- app.js
+++ app.js
@@ -1,64 +1,61 @@
 let form = document.querySelector("#form");
-const myWs = new WebSocket(`ws://${location.hostname}:9000`);
-const {
-    usermsg
-} = form;
+
+const { usermsg } = form;
 const chatbox = document.getElementById('chatbox')
-let audio = new Audio("wavs/beep.mp3");
 
 
-//функция получения текущей даты 
-function getDate(timeFromBack) {
-
-    let date = new Date(timeFromBack); //получаем форму с датой
-    let minutes = date.getMinutes(); // получаем минуты 
-    let hours = date.getHours(); // получаем часы
-    //проверка времени если цифра без ноля
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    hours = hours < 10 ? "0" + hours : hours;
-    return `${hours}:${minutes}`;
-
-}
-
-// функция отрисовки HTML на странице 
-function renderMessage(nameUser, textMessage, time) {
-    let classNameAdmin = nameUser == "Admin" ? " admin" : "";
-    chatbox.innerHTML += `
-    <div class="message${classNameAdmin}">
-       <div class="user-name"><b>${nameUser} ${ getDate(time)}</b> </div>
-      <div class="text">  ${textMessage} </div>
-    </div>`;
-
-    //поле ввода при добавленнии сообщения ползунок опускается в самый низ 
-    chatbox.scrollTop = chatbox.scrollHeight
-    audio.play();
-}
+const myWs = new WebSocket(`ws://${location.hostname}:9000`);
+myWs.onmessage = function(event) {
+    let messageObjec = JSON.parse(event.data);
+    (new Message(messageObjec.user, messageObjec.message, messageObjec.timestamp)).render();
+};
 
 
-
-
-
-
-
-
-
-
-function sendMessage(e) {
-    e.preventDefault();
-
-    if (!usermsg.value) return;
-
-    sendToServer(usermsg.value); // отправляем то, что передаем в input 
-    ;
-
-    //проверка на не пустую строку
-    if (usermsg.value) {
-        usermsg.value = "";
+class AudioMsg {
+    constructor(name) {
+        this.audio = new Audio(`wavs/${name}.mp3`);
     }
 
+    play() {
+        this.stop();
+        this.audio.play();
+    }
+
+    stop() {
+        this.audio.pause()
+        this.audio.currentTime = 0;
+    }
 }
 
-form.addEventListener("submit", sendMessage); // отправка 
+let audio = new AudioMsg("beep");
+let audioAdmin = new AudioMsg("beep2");
+
+
+class Clock {
+    constructor(timeFromBack) {
+        this.date = new Date(timeFromBack); //получаем форму с датой
+        this.minutes = this.date.getMinutes(); // получаем минуты
+        this.hours = this.date.getHours(); // получаем часы
+
+        this.minutes = this.minutes < 10 ? "0" + this.minutes : this.minutes;
+        this.hours = this.hours < 10 ? "0" + this.hours : this.hours;
+    }
+
+    getTimeNow(){
+        return `${this.hours}:${this.minutes}`;
+    }
+}
+
+
+
+
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    Message.send(usermsg.value);
+
+}); // отправка
 
 
 // отправка на сервер методом POST
@@ -80,12 +77,16 @@ function renderMessageFromServer() {
     }).then(messages => {
         if (messageLength < messages.length) { // проверка на отрисовку сразу после следующего сообщения 
             chatbox.innerHTML = "";
-            messages.forEach(message => renderMessage(message.user, message.message, message.timestamp));
+            messages.forEach(message => {
+                let obj = new Message(message.user, message.message, message.timestamp);
+                obj.render();
+            });
         }
         messageLength = messages.length;
 
     })
 }
+
 // сколько человек онлайн
 function renderUserOnline() {
     fetch("/count-users").then((response) => {
@@ -95,7 +96,48 @@ function renderUserOnline() {
 
 renderUserOnline();
 
-myWs.onmessage = function(event) {
-    let messageObjec = JSON.parse(event.data);
-    renderMessage(messageObjec.user, messageObjec.message, messageObjec.timestamp);
-};
+class Message {
+    /**
+     *
+     * @param {string} nameUser
+     * @param {string} textMessage
+     * @param {number} timeStamp
+     */
+    constructor(nameUser, textMessage, timeStamp) {
+        Object.assign(this, {nameUser, textMessage, timeStamp})
+
+
+    }
+
+    // функция отрисовки HTML на странице
+    /**
+     * rendering message.
+     */
+    render() {
+
+        chatbox.innerHTML += `
+    <div class="message${ this.nameUser === "Admin" ? " admin" : "" }">
+       <div class="user-name"><b>${ this.nameUser } ${ (new Clock(this.timeStamp)).getTimeNow() }</b> </div>
+      <div class="text">  ${ this.textMessage } </div>
+    </div>`;
+
+        //поле ввода при добавленнии сообщения ползунок опускается в самый низ
+        chatbox.scrollTop = chatbox.scrollHeight
+
+        audio.play();
+    }
+
+    static send(value) {
+
+        if (!usermsg.value) return;
+
+        sendToServer(usermsg.value); // отправляем то, что передаем в input
+
+        //проверка на не пустую строку
+        if (usermsg.value) {
+            usermsg.value = "";
+        }
+    }
+
+
+}
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const expres = require('express'); // подключаем express методом require 
+const express = require('express'); // подключаем express методом require
 const path = require('path'); // подключаем путь с пути 
 const absolutePath = path.resolve(__dirname); //абсолютный путь
 const bodyParser = require('body-parser'); //извлечение модулем body-parser HTTP post
 const WebSocket = require('ws'); // подключение сокета 
 const wsServer = new WebSocket.Server({ port: 9000 });
 
-const app = expres(); // инициализируем express записывая в переменную app, теперь app это обьект, 
+const app = express(); // инициализируем express записывая в переменную app, теперь app это обьект,
 //который имеет множество методов для работы с данным приложением
 
 
@@ -16,7 +16,7 @@ app.use(bodyParser.json()); // JSON и text
 
 
 //получаем файлы с node.js 
-app.get(/\w+\.(css|js|html)|^\/$/, (req, res) => {
+app.get(/\w+\.(css|js|html|jpg)|^\/$/, (req, res) => {
     res.sendFile(absolutePath + req.path);
 })
 
@@ -25,15 +25,12 @@ app.get(/\w+\.(mp3|wav)|^\/$/, (req, res) => {
     res.sendFile(absolutePath + req.path);
 })
 
-
-let messageBase = []; // внутреннее хранилище сообщений 
-
-
 app.get('/get-messages', (request, response) => { // Aдресс на который отсылаются сообщения 
     response.send(messageBase) //отправляем мы с backend на фронт( которая в функции renderMessageFromServer запросила)
 
 })
 
+let messageBase = []; // внутреннее хранилище сообщений
 
 
 async function startServer() {
@@ -53,46 +50,55 @@ const users = {}
 //
 wsServer.on('connection', onConnect);
 
+/**
+ * on client connect
+ * @param {WebSocket} wsClient
+ */
 function onConnect(wsClient) {
 
     users[wsClient._socket.remoteAddress] = wsClient;
-
-
     console.log("длинна массива " + Object.values(users).length);
-    console.log('Новый пользователь');
 
 
-    wsClient.on('close', function hui() {
+    //при закрытии с соеденинием клиента
+    wsClient.on('close', function quest() {
         delete users[wsClient._socket.remoteAddress];
-        console.log('Пользователь отключился');
-    });
-
-    wsClient.on('message', function(message) {
-        let userName = wsClient._socket.remoteAddress; //получение инициализированного ip
-
-        if (userName === '::1' || userName == "::ffff:127.0.0.1") { // проверка и перезапись имём в чате
-            userName = "Admin"
-        } else {
-            userName = "User (" + userName.split('.').pop() + ')' // удаляем последний элемент с массива 
-                //и возвращаем его
-        }
-
-        const messageObject = { // добавляем данные который приходят, в массив messageBase
-            user: userName,
-            message: message,
-            timestamp: +new Date(),
-        }
-
-        messageBase.push(messageObject);
-        console.log(messageBase);
-
-        let sockets = Object.values(users);
-        for (let socket of sockets) {
-            socket.send(JSON.stringify(messageObject));
-        }
 
     });
+    wsClient.on('message', message => onMessage(message, wsClient));
 
+}
+
+function onMessage (message, wsClient) {
+    let userName = wsClient._socket.remoteAddress; //получение инициализированного ip
+
+    if (userName === '::1' || userName == "::ffff:127.0.0.1") { // проверка и перезапись имём в чате
+        userName = "Admin"
+    } else {
+        userName = "User (" + userName.split('.').pop() + ')' // удаляем последний элемент с массива
+        //и возвращаем его
+    }
+
+    const messageObject = { // добавляем данные который приходят, в массив messageBase
+        user: userName,
+        message: message,
+        timestamp: +new Date(),
+    }
+
+    messageBase.push(messageObject);
+    sendMessageToUsers(messageObject);
+
+}
+
+/**
+ *
+ * @param {{user, message, timestamp}} messageObject
+ */
+function  sendMessageToUsers (messageObject){
+    let sockets = Object.values(users);
+    for (let socket of sockets) {
+        socket.send(JSON.stringify(messageObject));
+    }
 }
 
 //отсылаем количество юзеров на сайте на фронт
@@ -102,4 +108,3 @@ app.get("/count-users", (req, res) => {
 
 
 
-console.log('Сервер запущен на 9000 порту');
